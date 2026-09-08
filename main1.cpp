6243c2379d22cdb9dfc01f20284dfaabd940d0bc
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-08 19:08:50 +0000

    p3_letterH

--- main1.cpp
+++ main1.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 int main()
 {
-    std::cout<<"I love programming\n";
-    std::cout<<"It takes some time to practice,but I will achieve my goal"<<std::endl;
-    std::cout<<"I promise to best developer ever\n"<<std::endl;
-    std::cout<<"Best regards,\n";
-    std::cout<<"Aya Alaa Roshdy" <<std::endl;
+    std::cout<<"*      *\n";
+    std::cout<<"*      *"<<std::endl;
+    std::cout<<"********"<<std::endl;
+    std::cout<<"*      *\n";
+    std::cout<<"*      *" <<std::endl;
 return 0;
 
 }
