a495bf18063a9a4b51a13009291767990e26f99c
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-10 18:16:37 +0000

    escape sequense

---
+++ escape se.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Dear sir \\Madam \n";
+    cout<<"How are you ?\n";
+    cout<<"My name is \"Aya\" , nice to meet you.\n";
+    return 0 ;
+}
--- p4.cpp
+++ p4.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 int main()
 {
+    //كل ما كانت الكومنتات اقل ف البرنامج كل ما كان البرنامج اسرع
+    // this is my first programme
     cout<<"I progress in programming like a turtle\n";
     cout<<"it is so hard for me like climing a mountain"<<endl;
+    /*i learn how to print on the monitor 'i am afraid from this roud but 
+    i will try . no way for escaping*/
+    //السلاش n مبتفضيش البافر بعد كل امر طباعه 
+    /*endl بتفضى البافر بعض كل امر طباعه لو استخدمت ها بعد كل امر طباعه هتبطى 
+    البرنامج ولو استخدمت ان عالطول البفر يتملى لغايه ال
+    الاخر ويتقل البرنامج نقسمهم كل مجموعه طباعه بعدها امر endl واحد */
     return 0;
 }
