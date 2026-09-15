23670a1094b811ec3902ecf88aa7f6a325de6e4b
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-15 20:17:31 +0000

    variable types1

---
+++ p5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int myage=32;
+    float omar=7.6;
+    char mana='M';
+    double koko=21.8888888;
+    bool aya=1;
+    string yoyo ="omar.";
+    cout << omar<<endl;
+    cout << mana<<endl;
+    cout << koko<< endl;
+    cout <<aya << endl;
+
+
+    cout <<"my age is " << myage << " years old." <<endl;
+    myage=25;
+    cout<< myage <<endl;
+    return 0;
+}
