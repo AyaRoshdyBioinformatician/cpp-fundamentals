8ccaf8f3f4dd9e092bbd787ae0d88494a84f3575
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-17 03:28:34 +0000

    variables2

---
+++ p6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string Name= "Aya Alaa Amen" ;
+int Age=33;
+string Country= "Egypt";
+string City= "suez";
+int Salary= 500;
+int yearly_salary=12*Salary;
+char Gender='F';
+bool Married=1;
+cout<<"*************************\n";
+cout<< "Name : " << Name <<endl;
+cout<< "Age : " <<Age<< "\n";
+
+cout<< "Country :"<< Country<<endl;
+cout<< "city :"<<City<<"\n";
+cout<<"Salary:"<<Salary<<endl;
+cout <<"Yearly salary :"<<yearly_salary<<endl;
+cout << "Gender :"<< Gender<<"\n";
+cout <<"Married :" << Married<<endl;
+cout<<"**************************\n";
+return 0;
+
+
+
+
+}
