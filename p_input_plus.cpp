923af9a6e3f943425f8a51416fb66d5e178be867
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-21 07:42:11 +0000

    inputvariables homework18.2

---
+++ p_input_plus.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=20, b=30 ,c=10;
+    cout<<a<<" +\n";
+    cout<<b<<"+"<<endl;
+    cout<<c<<endl;
+    cout<<"_____________________"<<endl;
+    cout<<"Total= "<<a+b+c<<"\n";
+    
+    int Age=25;
+    cout<<"After 5 years you will be "<<Age+5<<"years old \n \n \n \n";
+
+    int num1;
+    int num2;
+    int num3;
+    cout<< "Please enter num1 "<<endl;
+    cin>>num1;
+    cout<<"Please enter num2 \n";
+    cin>>num2;
+    cout<<"please enter num3 \n";
+    cin>>num3;
+    cout<<num1<<"+\n";
+    cout<<num2<<"+\n";
+    cout<<num3<<"\n";
+    cout <<"________________"<<endl;
+    cout<<"total = "<<num1+num2+num3<<endl;
+    return 0;
+    
+    
+
+}
