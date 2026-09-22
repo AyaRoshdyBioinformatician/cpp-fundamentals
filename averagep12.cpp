089a64a407c230c8306715e8eb0bc3f233ba8361
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-22 13:38:57 +0000

    soluation_of_average

---
+++ averagep12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    short A ,B,C,Average;
+    cout <<"Enter mark 1"<<endl;
+    cin>>A;
+    cout<<"Enter mark 2 \n";
+    cin>>B;
+    cout <<"enter mark 3 \n";
+    cin>>C;
+    Average=(A+B+C)/3;
+    cout <<"Average of your marks = "<<Average <<endl;
+    return 0;
+    
+
+}
