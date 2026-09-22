c1d10f0874759283c4ac4b8df0ff73c63127e804
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-22 05:26:34 +0000

    sizes ofdatatype

---
+++ sizesdatatype.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<sizeof(bool)<<endl;
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(short)<<endl;
+    cout<<sizeof(long)<<endl;
+    cout<<sizeof(long long)<<endl;
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(double)<<endl;
+    cout<<sizeof(long double)<<endl;
+//return to video 19 to code function that give you data type range
+    return 0;
+
+}
