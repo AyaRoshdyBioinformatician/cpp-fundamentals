835ffcb60b05cb93c8af07b97f297e86bdcd21d4
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-22 14:11:42 +0000

    rec_area

---
+++ rectangle_area_p13.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    float a,b,rectangle_area;
+    cout <<"Please Enter length and width \n";
+    cin >>a>>b;
+    cout<<endl;
+    rectangle_area= a*b;
+    cout <<"Rectangle area = "<<rectangle_area<<endl;
+    return 0;
+
+
+}
