0337182875c1b8c3dea1b57b760effa595b68ac8
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-22 13:27:19 +0000

    sum

---
+++ sum3p12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    short A,B,C;
+    cout <<"enter num1 \n";
+    cin>>A;
+    cout <<"enter num2 \n";
+    cin>>B;
+    cout <<"enter num3 \n";
+    cin>>C;
+    cout <<"the sum ="<<A+B+C<<endl;
+    return 0;
+    
+}
