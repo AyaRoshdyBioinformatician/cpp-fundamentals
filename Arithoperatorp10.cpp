4d7fbdda60f8e1e75dc922e5b27023220c37a9f0
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-22 07:26:29 +0000

    input number and arithmatic opretor

---
+++ Arithoperatorp10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    short A,B;
+
+    cout<<"Please enter A \n ";
+
+    cin>>A;
+    cout<<"Please enter B \n ";
+    cin>>B;
+    cout<<"A+B= "<<A+B<<endl;
+    cout<<"A-B= "<<A-B <<endl;
+    cout<<"A*B= "<<A*B<<endl;
+    cout<<"A/B= " <<A/B<<endl;
+    cout<<"A module B = "<<A%B<<endl;
+    return 0;
+
+}
