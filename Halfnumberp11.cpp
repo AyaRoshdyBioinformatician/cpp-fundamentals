aa024168b59aedde3ed3b8ab6bbe720e62b2455e
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-22 11:45:48 +0000

    halfofnumberyouenter

---
+++ Halfnumberp11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Num ,halfofnum;
+    cout << "Please enter your num : \n ";
+    cin>> Num;
+    halfofnum=Num/2;
+    cout<<"Half of "<<Num<<" = "<<halfofnum<<endl;
+    return 0;
+
+
+    
+}
