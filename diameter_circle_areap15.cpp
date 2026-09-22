842ca014f7a392ef2ebe0ae43a971c368081bf72
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-22 15:41:50 +0000

    circle_area_by_diameter

---
+++ diameter_circle_areap15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    const double pi = 3.14;
+    double D,Area;
+    cout<<"Please Enter Circle diameter \n ";
+    cin>>D;
+    cout<<endl;
+    Area=(pi*D*D)/(4);
+    cout<<"Circle area = "<<Area<<endl;
+    return 0;
+    
+
+
+}
