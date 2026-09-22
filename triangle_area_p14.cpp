396c165340fef084b3bb5fae159bbe039ac4cb5d
AyaRoshdyBioinformatician <ayaa1381994@gmail.com>
2026-09-22 15:25:29 +0000

    triangleareawithbase

---
+++ triangle_area_p14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    double base,height,area;
+    cout<<"Please enter the base and the height of the triangle\n base then height \n";
+    cin >>base>>height;
+    area= (base/2)*height;
+    cout <<endl;
+    cout<<"Area of Triangle = "<<area<<endl;
+    return 0;
+    
+    
+}
